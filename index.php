be8be5103c11a8c9c607f4be2a60fad56681d798
VanhellsingX <46110836+VanhellsingX@users.noreply.github.com>
2018-12-26 12:35:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,38 +7,26 @@ $method = $_SERVER['REQUEST_METHOD'];
 /*************************************/
 /* RECIBO PARAMETROS POR METODO POST */
 /*************************************/
-if($method == 'POST'){
+//if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
 	$text = $json->queryResult->parameters->text;
+	
+	$intent = $json->intent->name;	
+	$evento = $json->intent->displayName;
+	$idioma = $json->languageCode;
 
 	switch ($text) {
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
-
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
-
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
-		
 		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			$speech = "PRUEBA DE RESPUESTA DE EVENTO USANDO EL JSON V2 QUE SUPO A GOBIERNO DE SANTOS APLICARLO.";
 			break;
 	}
 
 	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($response);
-}
-else
-{
-	echo "METODO NO SOPORTADO";
-}
+	$responsemsg = new \stdClass();
+	
+	$response->payload->google->expectUserResponse = false;
+	$response->payload->google->richResponse->items[0]->simpleResponse->textToSpeech=$speech;
+
+echo json_encode($response);
 ?>
